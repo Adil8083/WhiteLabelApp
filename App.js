1f7ccf30aadc10bada417dde8d74c5d0420a6f4f
Adil Waheed <adilwahed@outlook.com>
2020-09-14 18:09:22 +0500

    Redefined file structure and installed navigator

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-import ActorWEScreen from "./screens/ActorWEScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import ActorWEScreen from "./src/screens/ActorWEScreen";
+import Splash from "./src/screens/Splash";
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <ActorWEScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ActorScreen">
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="ActorScreen" component={ActorWEScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
