660e2bbb22dd0df98c159fe7005791b1cdf76709
Adil Waheed <adilwahed@outlook.com>
2020-09-25 14:21:53 +0500

    update AppButon and AppText

--- src/components/AppButton.js
+++ src/components/AppButton.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import colors from "../config/colors";
 
-function AppButton({ title, onPress, color = "primary" }) {
+function AppButton({ title, onPress, styleButton }) {
   const navigation = useNavigation();
   return (
     <TouchableOpacity
-      style={[styles.button, { backgroundColor: colors[color] }]}
+      style={[styles.button, { ...styleButton }]}
       onPress={onPress}
     >
       <Text style={styles.text}>{title}</Text>
