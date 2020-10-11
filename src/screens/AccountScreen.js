82b136de81ee04bca892304443818ab0d4bdf86b
Rasib Nadeem <raseebnadeem@gmail.com>
2020-10-11 23:02:25 +0500

    Account Screen updated

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,16 +1,44 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
 import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import AccountItems from "../components/AccountItems";
+import { Theme } from "../constants/Theme";
+import TextSize from "../constants/TextSize";
 
 const AccountScreen = () => {
   return (
     <Screen>
-      <Text>AccountScreen</Text>
+      <ListItem
+        title="James"
+        subTitle="jamesbond007@gmail.com"
+        image={require("../../assets/profile.jpg")}
+      />
+      <AccountItems
+        title="Settings"
+        icon={
+          <MaterialIcons
+            name="settings"
+            size={TextSize.iconSize}
+            color={Theme.spareColor}
+          />
+        }
+        onPress={() => console.log("Settings")}
+      />
+      <AccountItems
+        title="Log Out"
+        icon={
+          <MaterialCommunityIcons
+            name="logout"
+            size={TextSize.iconSize}
+            color={Theme.spareColor}
+          />
+        }
+        onPress={() => console.log("Log Out")}
+      />
     </Screen>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default AccountScreen;
