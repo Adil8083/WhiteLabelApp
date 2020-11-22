4cdcdda9f4ba08a01911a0bc437003b2086784fb
uzirnaseem <uzair12naseem@gmail.com>
2020-11-22 17:09:57 +0500

    added politician api component

--- src/constants/Screens.js
+++ src/constants/Screens.js
@@ -19,12 +19,9 @@ export const SCREENS = {
   SportsInfo: "SportsInfoScreen",
   SportsAchievements: "SportsAchievementsScreen",
   SocialAccounts: "SocialAccounts",
-<<<<<<< HEAD
   PoliticianInfo: "PoliticianInfo",
   PoliticianAchievement: "politicianAchievement",
   PoliticianEducation: "politicianEducation",
   PoliticianProjects: "politicianProjects",
-=======
   GenerateApk: "GenerateApk",
->>>>>>> 2c85380319dfa00595c08384e554727f48f005df
 };
