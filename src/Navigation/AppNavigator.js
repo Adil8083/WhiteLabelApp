a74d863148e431d0a198c50c0c39d38585541ecd
Rasib Nadeem <raseebnadeem@gmail.com>
2020-11-24 17:36:33 +0500

    Changes in modals

--- src/Navigation/AppNavigator.js
+++ src/Navigation/AppNavigator.js
@@ -28,7 +28,7 @@ const Stack = createStackNavigator();
 const AppNavigator = () => {
   return (
     <Stack.Navigator
-      initialRouteName={SCREENS.FootBallStatistics}
+      initialRouteName={SCREENS.NamingApp}
       screenOptions={{
         headerShown: false,
       }}
